ec49a2b25e87211ae706d82bd95abcb951b6f56c
Amit kumar <49529841+amitec9@users.noreply.github.com>
2021-04-02 21:53:32 +0530

    Add files via upload

---
+++ String/Day_28_CountPalindromicSubsequences.cpp
@@ -0,0 +1,37 @@
+// C++ program to counts Palindromic Subsequence
+#include <bits/stdc++.h>
+using namespace std;
+int n, dp[1000][1000];
+string str = "abcb";
+int countPS(int i, int j)
+{
+
+	if (i > j)
+		return 0;
+
+	if (dp[i][j] != -1)
+		return dp[i][j];
+
+	if (i == j)
+		return dp[i][j] = 1;
+
+	else if (str[i] == str[j])
+		return dp[i][j]
+			= countPS(i + 1, j) +
+				countPS(i, j - 1) + 1;
+
+	else
+		return dp[i][j] = countPS(i + 1, j) +
+						countPS(i, j - 1) -
+						countPS(i + 1, j - 1);
+}
+
+// Driver code
+int main()
+{
+	memset(dp, -1, sizeof(dp));
+	n = str.size();
+	cout << "Total palindromic subsequence are : "<< countPS(0, n - 1) << endl;
+	return 0;
+}
+
